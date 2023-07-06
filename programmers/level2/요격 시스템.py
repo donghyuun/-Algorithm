6c03e6c59834be3e7df65e03611b96773c633587
Hyun <82355395+donghyuun@users.noreply.github.com>
2023-07-06 19:46:08 +0900

    Update 요격 시스템.py

--- programmers/level2/요격 시스템.py
+++ programmers/level2/요격 시스템.py
@@ -1,7 +1,7 @@
 def solution(targets):
     answer=0
     pivot=0
-    targets.sort(key=lambda x: (x[0],x[1]))
+    targets.sort(key=lambda x: x[1])
     
     for t in targets:
         if pivot <= t[0]:
